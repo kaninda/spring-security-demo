98d3fc81f469933e8fc7a507fadb2c949cd8ca71
arnaudkaninda <kanindaarnaud@gmail.com>
2024-10-10 07:54:48 +0200

    Security: Mise en place de formLogin et httpBasic

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# spring-security-demo
+# Spring-security-demo
 This repo we will study spring security
+
+## Command pour HttpBasic
+Affichage de tous les books en local
+```shell
+curl http://localhost:8081/api/books -u userTest:password  -v
+```
+Ajout des books dans la base
+
+```
+curl -i -X POST -u "userTest:password" -H "Content-type:application/json" -d @- http://localhost:8081/api/books <<EOF
+{
+  "title": "The Great Gatsby",
+  "author": "F. Scott Fitzgerald"
+}
+EOF
+```
+
+
--- src/main/java/com/aka/security/configuration/SecurityConfig.java
+++ src/main/java/com/aka/security/configuration/SecurityConfig.java
@@ -2,19 +2,36 @@ package com.aka.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(
-                auth -> {
-                   auth.requestMatchers("/api/books").authenticated();
-                   auth.requestMatchers("/status").permitAll();
-                }
-        ).build();
+                        auth -> {
+                            auth.requestMatchers("/api/books").authenticated();
+                            auth.requestMatchers("/status").permitAll();
+                        }
+                ).formLogin(
+                        Customizer.withDefaults()
+                ).httpBasic(
+                        Customizer.withDefaults()
+                ).csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager(new User("userTest","{noop}password", Collections.emptyList()));
     }
 }
